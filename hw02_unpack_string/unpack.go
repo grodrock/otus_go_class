527fcab86090bfab494ec606c08bd556a201cb61
Markin Andrey <markin.andrey.v@gmail.com>
2021-11-28 18:51:52 +0300

    Implemented Unpack function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,62 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	// fmt.Printf("inp str: %s (% x)\n", s, s)
+
+	var prevRune rune
+	var resBuilder strings.Builder
+	isEscape := false
+
+	for _, r := range s {
+		// fmt.Printf("%d: q=%q, v=%v\n", ri, r, r)
+
+		// заэкранировать можно только цифру или слэш
+		if isEscape && r != '\\' && !unicode.IsDigit(r) {
+			return "", ErrInvalidString
+		}
+
+		// isDigit
+		if unicode.IsDigit(r) && !isEscape {
+
+			if prevRune == 0 {
+				return "", ErrInvalidString
+			}
+
+			digit := int(r - '0')
+			for i := 0; i < digit; i++ {
+				resBuilder.WriteRune(prevRune)
+			}
+			prevRune = 0
+
+		} else {
+
+			if prevRune > 0 && !isEscape {
+				resBuilder.WriteRune(prevRune)
+			}
+
+			if r == '\\' && !isEscape {
+				isEscape = true
+			} else {
+				isEscape = false
+				prevRune = r
+			}
+
+		}
+
+	}
+	// last rune
+	if prevRune > 0 {
+		resBuilder.WriteRune(prevRune)
+	}
+
+	resultStr := resBuilder.String()
+	// fmt.Println(resultStr)
+	return resultStr, nil
 }
