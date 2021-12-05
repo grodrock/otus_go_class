43e3fce9f19120d5dd7d63b899cb88b61f03e622
Markin Andrey <markin.andrey.v@gmail.com>
2021-12-05 13:03:05 +0300

    implemented Top10 func

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,46 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	wordsCountMap := make(map[string]int)
+
+	wordsList := strings.Fields(text)
+
+	// create map of words
+	for _, word := range wordsList {
+		if _, ok := wordsCountMap[word]; ok {
+			wordsCountMap[word]++
+			continue
+		}
+		wordsCountMap[word] = 1
+	}
+	fmt.Println(wordsCountMap)
+
+	// find top 10
+	// - create slice of words
+	wordsSlice := make([]string, 0, len(wordsCountMap))
+	for word := range wordsCountMap {
+		wordsSlice = append(wordsSlice, word)
+	}
+	fmt.Println(wordsSlice, len(wordsSlice))
+
+	// - sort slice with map data
+	sort.Slice(wordsSlice, func(i, j int) bool {
+		if wordsCountMap[wordsSlice[i]] != wordsCountMap[wordsSlice[j]] {
+			return wordsCountMap[wordsSlice[i]] > wordsCountMap[wordsSlice[j]]
+		}
+		return strings.Compare(wordsSlice[i], wordsSlice[j]) < 0
+	})
+
+	// - return top 10 or len of slice
+	returnSize := 10
+	if len(wordsSlice) < returnSize {
+		returnSize = len(wordsSlice)
+	}
+	return wordsSlice[:returnSize]
 }
