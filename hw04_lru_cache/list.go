23b021059a373b779cb85060c2fcd2ee858cf9a1
Markin Andrey <markin.andrey.v@gmail.com>
2021-12-18 13:43:38 +0300

    реализован двусвязный список

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,91 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	size                int
+	frontItem, backItem *ListItem
+}
+
+// длина списка.
+func (l *list) Len() int {
+	return l.size
+}
+
+// первый элемент списка.
+func (l *list) Front() *ListItem {
+	return l.frontItem
+}
+
+// последний элемент списка.
+func (l *list) Back() *ListItem {
+	return l.backItem
+}
+
+// добавить значение в начало.
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := ListItem{
+		Value: v,
+		Next:  l.frontItem,
+	}
+
+	if l.backItem == nil {
+		l.backItem = &newItem
+	}
+	if l.frontItem != nil {
+		l.frontItem.Prev = &newItem
+	}
+
+	l.frontItem = &newItem
+	l.size++
+
+	return &newItem
+}
+
+// добавить значение в конец.
+func (l *list) PushBack(v interface{}) *ListItem {
+	newItem := ListItem{
+		Value: v,
+		Prev:  l.backItem,
+	}
+	if l.frontItem == nil {
+		l.frontItem = &newItem
+	}
+	if l.backItem != nil {
+		l.backItem.Next = &newItem
+	}
+
+	l.backItem = &newItem
+	l.size++
+
+	return &newItem
+}
+
+// удалить элемент.
+func (l *list) Remove(i *ListItem) {
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.backItem = i.Prev
+	}
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.frontItem = i.Prev
+	}
+	l.size--
+}
+
+// переместить элемент в начало.
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil {
+		// мы и так первые.
+		return
+	}
+
+	i.Prev.Next = i.Next
+	i.Next = l.frontItem
+	i.Prev = nil
+	l.frontItem.Prev = i
+	l.frontItem = i
 }
 
 func NewList() List {
