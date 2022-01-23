83ccf8eb445d4052145cd8ceaa480d266011b781
Markin Andrey <markin.andrey.v@gmail.com>
2022-01-23 18:51:44 +0300

    Отработка пайплайна без возможности остановки

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -6,9 +6,19 @@ type (
 	Bi  = chan interface{}
 )
 
+// Stage - функция, которая принимает канал со значениями,
+// выполняет над ними какую-то работу и возвращает канал с результатами
+// этой функции.
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// Значения из канала in передаются в первый этап Stage,
+	// результат которой - входной канал для следующего этапа Stage
+
+	inNext := in // канал данных для следующего этапа
+
+	for _, stage := range stages {
+		inNext = stage(inNext)
+	}
+	return inNext
 }
