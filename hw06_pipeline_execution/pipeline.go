6dcdc5292b8351535aa4d58faa12a21985abcf70
A.Markin <A.Markin@x5.ru>
2022-01-24 13:49:58 +0300

    Отработка пайплайна с остановкой

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -11,6 +11,30 @@ type (
 // этой функции.
 type Stage func(in In) (out Out)
 
+// getResultChannel - функция, которая возвращает канал с результатами пайплайна,
+// закрывает его по окончании или при получении сигнала остановки.
+func getResultChannel(in In, done In) Out {
+	resChannel := make(chan interface{})
+
+	go func() {
+		for {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					close(resChannel)
+					return
+				}
+				resChannel <- v
+			case <-done:
+				close(resChannel)
+				return
+			}
+		}
+	}()
+
+	return resChannel
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Значения из канала in передаются в первый этап Stage,
 	// результат которой - входной канал для следующего этапа Stage
@@ -20,5 +44,8 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	for _, stage := range stages {
 		inNext = stage(inNext)
 	}
-	return inNext
+
+	resultChannel := getResultChannel(inNext, done)
+
+	return resultChannel
 }
