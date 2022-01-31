417004e59f6173b81c61d5aee8977a9a91e497ec
Markin Andrey <markin.andrey.v@gmail.com>
2022-01-31 09:50:54 +0300

    тесты на ошибки

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"os"
 )
 
 var (
@@ -9,7 +10,27 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+// Copy копирует данные из файла fromPath в toPath
+//
+// offset - смещение, скоторого читаем файл, по умолчанию 0
+// при превышении размера файла возвращает ошибку ErrOffsetExceedsFileSize;
+// limit - количество байт для считывания из файла, по умолчанию 0 - весь файл
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+
+	// проверка превышения смещением размера файла
+	fInfo, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if offset > fInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	file, err := os.Open(fromPath)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,22 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+
+	t.Run("file not exist check", func(t *testing.T) {
+		err := Copy("NotExistFile", "out", 0, 0)
+		require.Errorf(t, os.ErrNotExist, err.Error())
+	})
+
+	t.Run("offset exceeds FileSize", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out", 1<<20, 0)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	})
 }
