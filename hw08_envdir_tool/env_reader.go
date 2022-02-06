71d0f621d7074a977aeaccc45cbcf1483ec64e2e
A.Markin <A.Markin@x5.ru>
2022-02-06 17:12:56 +0300

    env reader done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +17,52 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment, 0)
+	dirEntries, err := os.ReadDir(dir)
+	if err != nil {
+		return env, err
+	}
+	var fname string
+	for _, entry := range dirEntries {
+		if entry.IsDir() {
+			continue
+		}
+		fname = entry.Name()
+		envval, err := ReadEnvFile(filepath.Join(dir, fname))
+		if err == nil {
+			env[fname] = envval
+		}
+
+	}
+	return env, nil
+}
+
+// ReadEnvFile reads file and return EnvValue
+func ReadEnvFile(fname string) (EnvValue, error) {
+	var envval EnvValue
+
+	fData, err := os.ReadFile(fname)
+	if err != nil {
+		return envval, err
+	}
+	if len(fData) == 0 {
+		envval.NeedRemove = true
+		return envval, err
+	}
+
+	var resBuilder strings.Builder
+	for _, r := range fData {
+
+		if r == 0 {
+			resBuilder.WriteString("\n")
+			continue
+		}
+		if r == 10 || r == 13 { // LF, CR
+			break
+		}
+		resBuilder.WriteByte(r)
+	}
+	envval.Value = strings.TrimRight(resBuilder.String(), "\t ")
+
+	return envval, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,59 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("read env dir", func(t *testing.T) {
+		env, err := ReadDir("testdata/env")
+		require.ErrorIs(t, nil, err)
+		require.Equal(t, env["BAR"], EnvValue{"bar", false})
+		require.Equal(t, env["HELLO"], EnvValue{"\"hello\"", false})
+		require.Equal(t, env["EMPTY"], EnvValue{"", false})
+		require.Equal(t, env["UNSET"], EnvValue{"", true})
+	})
+}
+func TestReadEnvFile(t *testing.T) {
+	t.Run("not exist file", func(t *testing.T) {
+		_, err := ReadEnvFile("testdata/env/not_here")
+		require.Error(t, err)
+	})
+
+	t.Run("file with second line", func(t *testing.T) {
+		env, err := ReadEnvFile("testdata/env/BAR")
+		require.Equal(t, "bar", env.Value)
+		require.Equal(t, false, env.NeedRemove)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("simple hello", func(t *testing.T) {
+		env, err := ReadEnvFile("testdata/env/HELLO")
+		require.Equal(t, "\"hello\"", env.Value)
+		require.Equal(t, false, env.NeedRemove)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("file with x00 separator", func(t *testing.T) {
+		env, err := ReadEnvFile("testdata/env/FOO")
+		require.Equal(t, "   foo\nwith new line", env.Value)
+		require.Equal(t, false, env.NeedRemove)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("empty file with symbols", func(t *testing.T) {
+		env, err := ReadEnvFile("testdata/env/EMPTY")
+		require.Equal(t, "", env.Value)
+		require.Equal(t, false, env.NeedRemove)
+		require.Equal(t, nil, err)
+	})
+
+	t.Run("empty file with zero length", func(t *testing.T) {
+		env, err := ReadEnvFile("testdata/env/UNSET")
+		require.Equal(t, "", env.Value)
+		require.Equal(t, true, env.NeedRemove)
+		require.Equal(t, nil, err)
+	})
 }
