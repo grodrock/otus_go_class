dc48cfeaaf556b3263f520fc0086282e0b445fff
A.Markin <A.Markin@x5.ru>
2022-02-07 14:04:11 +0300

    executor added

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Println("Not enough args")
+		os.Exit(1)
+	}
+	envDir := os.Args[1]
+	cmd := os.Args[2:]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ret := RunCmd(cmd, env)
+	os.Exit(ret)
 }
