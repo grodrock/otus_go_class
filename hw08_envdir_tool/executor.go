e7fbcf7c6015b257e7fbd5d281922e2643d7bfb3
A.Markin <A.Markin@x5.ru>
2022-02-07 14:04:11 +0300

    executor added

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+
+	command := exec.Command(cmd[0])
+	command.Env = append(os.Environ())
+	fmt.Println("command", command)
+	if len(cmd) > 1 {
+		command.Args = cmd[1:]
+		fmt.Println("command.Args", command.Args)
+	}
+	command.Stdout = os.Stdout
+	if err := command.Run(); err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	return 0
 }
